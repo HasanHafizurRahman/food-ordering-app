e71d19c0470884635bfdf43c7998d6415c91081c
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-11-05 11:48:37 +0600

    props drilling

--- src/components/header/body/Body.js
+++ src/components/header/body/Body.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card/Card';
+import './body.css';
 
 const Body = () => {
   return (
@@ -8,7 +9,19 @@ const Body = () => {
         Search
       </div>
       <div className='res-container'>
-        <Card />
+        <Card name="Bismillah Burger" rating="4.4" price="₹50" duration="44 mins" />
+        <Card name="ginshin Burger" rating="4.3" price="₹70" duration="124 mins" />
+        <Card name="Mayer Dua Food Corner" rating="4.4" price="₹40" duration="34 mins" />
+        <Card name="Mayer Dua Food Corner" rating="4.3" price="₹40" duration="34 mins" />
+        <Card name="ginshin Burger" rating="4.3" price="₹70" duration="124 mins" />
+        <Card name="Bismillah Burger" rating="4.4" price="₹50" duration="44 mins" />
+        <Card name="ginshin Burger" rating="4.3" price="₹70" duration="124 mins" />
+        <Card name="Mayer Dua Food Corner" rating="4.4" price="₹40" duration="34 mins" />
+        <Card name="Mayer Dua Food Corner" rating="4.5" price="₹40" duration="34 mins" />
+        <Card name="ginshin Burger" rating="4.3" price="₹70" duration="124 mins" />
+        <Card name="Ginshin Burger" rating="4.4" price="₹50" duration="44 mins" />
+        <Card name="Mayer Dua Food Corner" rating="4.4" price="₹40" duration="34 mins" />
+        <Card name="Bismillah Burger" rating="4.4" price="₹50" duration="44 mins" />
       </div>
     </div>
   );
--- src/components/header/body/Card/Card.js
+++ src/components/header/body/Card/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './card.css';
 
-const Card = () => {
+const Card = ({ name, rating, price, duration }) => {
   return (
     <div className='res-card'>
       <img src='https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg' alt='food-img' />
-      <h3>Bismillah Burger</h3>
+      <h3>{name}</h3>
       <h4>Fast Food, Fast Casual</h4>
-      <h4>4.4 stars</h4>
-      <h4>₹50</h4>
-      <h4>44 mins</h4>
+      <h4>{rating}</h4>
+      <h4>{price}</h4>
+      <h4>{duration}</h4>
     </div>
   );
 };
